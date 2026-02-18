66390e606496dbfb5d7ccf29f7b81a26a2240741
Purrlight <81241839+Purrlightt@users.noreply.github.com>
2026-02-18 13:59:30 +0100

    Add files via upload

---
+++ assets/changelog.js
@@ -0,0 +1,39 @@
+const OMEGA_CHANGELOG = [
+    {
+        "version": "v1.1.2",
+        "date": "2026-02-18",
+        "changes": [
+            "Changed the Tutorial layout.",
+            "Changed the Roblox Users icons to diffrent ones, since roblox Profile Icons didn't load",
+            "Added a 'Ingame Name' text box so we know who sent it.",
+            "Added the ability to Upload Images/Videos for tutorials or Bug Reports.",
+            "Added gradients to all the filters in the Rune tab.",
+            "Fixed the Changelogs flickering on selection",
+        ]
+    },
+    {
+        "version": "v1.1.1",
+        "date": "2026-02-16",
+        "changes": [
+            "Fixed a Security Vulnerability."
+        ]
+    },
+    {
+        "version": "v1.1.0",
+        "date": "2026-02-15",
+        "changes": [
+            "Added Changelog tab.",
+            "Added Feedback system.",
+            "Added Rune Cap toggle (Max / Reborn 5)."
+        ]
+    },
+    {
+        "version": "v1.0.0",
+        "date": "2026-02-08",
+        "changes": [
+            "Initial release of Rune Hub.",
+            "Added Rune Calculator.",
+            "Added Event Runes."
+        ]
+    }
+];
